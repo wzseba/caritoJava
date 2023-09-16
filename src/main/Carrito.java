2eb3cae6736def54df56b45cff3cbce032220e20
wzseba <wzseba@gmail.com>
2023-09-16 10:58:33 -0300

    add: se agegaron metodos para total de precios con y sin categoria

--- src/main/Carrito.java
+++ src/main/Carrito.java
@@ -3,11 +3,38 @@ package main;
 import java.util.ArrayList;
 
 public class Carrito {
-	
+
 	private ArrayList<Producto> productos;
-	
-	public Carrito(){
+
+	public Carrito() {
 		productos = new ArrayList<Producto>();
+	}
+
+	public int getCantidadProductos() {
+		return productos.size();
+	}
+
+	public void agregarProducto(Producto p) {
+		productos.add(p);
+	}
+
+	public double getPrecioTotal() {
+		double precioTotal = 0;
+
+		for (Producto producto : productos) {
+			precioTotal += producto.getPrecio();
+		}
+		return precioTotal;
+	}
+
+	public double getPrecioTotal(Categoria categoria) {
+		double precioTotal = 0;
+
+		for (Producto producto : productos) {
+			if(producto.getCategoria().equals(categoria))
+			precioTotal += producto.getPrecio();
+		}
+		return precioTotal;
 	}
 
 }
